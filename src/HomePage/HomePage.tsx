d036c2e314f0cedb36615409a0dd714ae6f2369b
leadstar116 <supremedev116@gmail.com>
2020-07-16 04:57:02 +1000

    update userlist

--- src/HomePage/HomePage.tsx
+++ src/HomePage/HomePage.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../_components/Header'
+import UserList from '../_components/UserList'
 
 function HomePage() {
     return (
         <div>
             <Header />
+            <UserList />
         </div>
     )
 }
---
+++ src/_components/UserList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserList = () => {
+    return (
+        <div className="p-2">
+            <h2>UserList</h2>
+        </div>
+    )
+}
+
+export default UserList
