482f52d3d9aea43a9d973ff065163a27de42e79b
leadstar116 <supremedev116@gmail.com>
2020-07-17 01:27:15 +1000

    add settings interface constants

---
+++ src/_constants/settings.interface.ts
@@ -0,0 +1,11 @@
+export interface LocationSettings {
+    nationality: LocationEnum
+}
+
+export enum LocationEnum {
+    Any = "",
+    CH = "CH",
+    ES = "ES",
+    FR = "FR",
+    GB = "GB",
+}
