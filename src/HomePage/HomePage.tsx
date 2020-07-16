b81c55c4ab5a682d95b7d30873166a95ded62e03
leadstar116 <supremedev116@gmail.com>
2020-07-16 23:55:22 +1000

    add search functionality

--- src/HomePage/HomePage.tsx
+++ src/HomePage/HomePage.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../_components/Header'
 import UserList from '../_components/UserList'
 
 function HomePage() {
+    const [searchString, setSearchString] = useState("")
+
     return (
         <div>
-            <Header />
-            <UserList />
+            <Header
+                searchString = {searchString}
+                setSearchString = {setSearchString}
+            />
+            <UserList
+                searchString = {searchString}
+            />
         </div>
     )
 }
