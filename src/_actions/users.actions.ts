a7c917ba903635c22beafa72c69e95be0ec1dfac
leadstar116 <supremedev116@gmail.com>
2020-07-17 03:50:43 +1000

    preload users in idle time

--- src/_actions/users.actions.ts
+++ src/_actions/users.actions.ts
@@ -11,3 +11,15 @@ export const clearUsers = () => ({
     type: CLEAR_USERS,
     payload: { }
 })
+
+export const UPDATE_USERS_WITH_PRELOADED_USERS = "UPDATE_USERS_WITH_PRELOADED_USERS"
+export const updateUsersWithPreloadedUsers = () => ({
+    type: UPDATE_USERS_WITH_PRELOADED_USERS,
+    payload: { }
+})
+
+export const UPDATE_PRELOADED_FLAG = "UPDATE_PRELOADED_FLAG"
+export const updatePreloadedFlag = (flag: boolean) => ({
+    type: UPDATE_PRELOADED_FLAG,
+    payload: { flag }
+})
