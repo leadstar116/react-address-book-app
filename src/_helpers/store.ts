e05c11391dafd9d852395becb6458af2a52711db
leadstar116 <supremedev116@gmail.com>
2020-07-25 04:03:27 +1000

    add dev tool

--- src/_helpers/store.ts
+++ src/_helpers/store.ts
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import thunkMiddleware from 'redux-thunk'
 import persistedReducer from '../_reducers'
 
 const store = createStore(
     persistedReducer,
-    applyMiddleware(
-        thunkMiddleware
+    composeWithDevTools(
+        applyMiddleware(thunkMiddleware)
     )
 );
 
